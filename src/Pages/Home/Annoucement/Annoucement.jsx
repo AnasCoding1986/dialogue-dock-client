cedf76e8fe4c90a60bb9af5d5138be3a138a7c16
AnasCoding1986 <anassust.scw@gmail.com>
2024-06-07 10:12:22 +0600

    annoucement section hide and block added

--- src/Pages/Home/Annoucement/Annoucement.jsx
+++ src/Pages/Home/Annoucement/Annoucement.jsx
@@ -2,23 +2,24 @@ import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import useAnnoucement from "../../../Hooks/useAnnoucement";
 import AnnoucementCard from "./AnnoucementCard";
 
-
 const Annoucement = () => {
-
     const [notification] = useAnnoucement();
+    const hasNotifications = notification.length > 0;
 
     return (
-        <div className="p-20">
+        <div className={`p-20 bg-[#ebf6fa] ${hasNotifications ? 'block' : 'hidden'}`}>
             <SectionTitle
                 heading="Annoucement"
                 subHeading="Please go through our annoucement"
             ></SectionTitle>
             <div className="grid gap-5">
                 {
-                    notification.map(singleNotification => <AnnoucementCard
-                        singleNotification={singleNotification}
-                        key={singleNotification._id}
-                    ></AnnoucementCard>)
+                    notification.map(singleNotification => (
+                        <AnnoucementCard
+                            singleNotification={singleNotification}
+                            key={singleNotification._id}
+                        ></AnnoucementCard>
+                    ))
                 }
             </div>
         </div>
